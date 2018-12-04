91dc580c6fc4b7d7a0142a13f0a802711355264c
Victor <victor.ananias8@gmail.com>
2018-12-04 15:28:59 -0200

    responsive columns added

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -10,37 +10,58 @@ import { DatePipe } from '@angular/common';
 export class ListComponent implements OnInit {
   mediaList: Media[] = [];
   columns: Media[][] = [];
+  date =  new Date();
+  itemsCount = 15;
+  columnsNumber = 3;
+
   constructor(
     private service: AppService,
     private datePipe: DatePipe
   ) { }
 
   ngOnInit() {
-    const date = new Date();
-    date.setDate(date.getDate() - 14);
-    console.log(this.datePipe.transform(date, 'yyyy-MM-dd') );
+    this.previous();
+  }
 
-    this.service.getMedia({start_date: this.datePipe.transform(date, 'yyyy-MM-dd') })
-      .subscribe((mediaList: Media[]) => {
-        console.log(mediaList);
+  next() {
+    this.nextDate();
+    this.loadPictures();
+  }
 
+  previous() {
+    this.previousDate();
+    this.loadPictures();
+  }
+
+  private nextDate() {
+    this.date.setDate(this.date.getDate() + this.itemsCount);
+  }
+
+  private previousDate() {
+    this.date.setDate(this.date.getDate() - this.itemsCount);
+  }
+
+  loadPictures() {
+    this.service.getMedia({
+      start_date: this.datePipe.transform(this.startDate, 'yyyy-MM-dd') ,
+      end_date: this.datePipe.transform(this.endDate, 'yyyy-MM-dd') ,
+    }).subscribe((mediaList: Media[]) => {
         this.mediaList = mediaList.sort((a, b) => b.date.localeCompare(a.date));
-
         this.generateColumns();
-      });
+    });
   }
 
   generateColumns() {
     const p = 0;
-    const columnsNumber = 3;
+    this.columns = [];
     this.columns[p] = [];
 
-    for (let column = 0; column < columnsNumber; column++ ) {
+    for (let column = 0; column < this.columnsNumber; column++ ) {
       this.columns[column] = [];
     }
 
     for (let i = 0; i < this.mediaList.length; i++ ) {
-      const column = ((i + 1) % columnsNumber || columnsNumber) - 1;
+      const column = ((i + 1) % this.columnsNumber || this.columnsNumber) - 1;
       this.columns[column].push(this.mediaList[i]);
     }
   }
@@ -53,6 +74,42 @@ export class ListComponent implements OnInit {
     const videoId = matches[1];
     const thumbnailNumber = 'maxresdefault.jpg';
     return `http://img.youtube.com/vi/${videoId}/${thumbnailNumber}`;
+  }
+
+  get startDate() {
+    return  this.datePipe.transform(this.date, 'yyyy-MM-dd');
+  }
+
+  get endDate() {
+    const date = new Date(this.startDate);
+
+    date.setDate(this.date.getDate() + this.itemsCount );
+
+    return  this.datePipe.transform(
+      date,
+      'yyyy-MM-dd'
+    );
+  }
+
+  resize(event) {
+    const windowWidth = event.target.innerWidth;
+    if (windowWidth > 1200) {
+      console.log('opa 1');
+
+      this.columnsNumber = 3;
+    } else if (windowWidth < 1200 && windowWidth >  700) {
+      console.log('opa 2');
+      this.columnsNumber = 2;
+    } else if (windowWidth <  700) {
+      console.log('opa 3');
+      this.columnsNumber = 1;
+    }
+
+    this.generateColumns();
+  }
+
+  get flex () {
+    return (100 / this.columnsNumber) + '%';
   }
 
 
