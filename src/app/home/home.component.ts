714b6840f20d5c20564fd4332bbd270bde7d6914
Victor <victor.ananias8@gmail.com>
2018-12-27 15:07:46 -0200

    save

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,10 +11,12 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class HomeComponent implements OnInit {
   mediaList: Media[] = [];
   date = new Date();
+  loadingMessage = 'Starting App';
+
   itemsCount = 15;
   maxColumnWidth = 400;
-  loadingMessage;
-  private _imgsDownloaded = 0;
+
+  private _downloadedImgs = 0;
 
   constructor(
     private router: Router,
@@ -29,123 +31,128 @@ export class HomeComponent implements OnInit {
         if (params.date) {
           this.date = new Date(params.date);
         }
-    });
 
-    this.previous();
-  }
-
-  next() {
-    this.nextDate();
-    this.getMediaList();
-  }
-
-  previous() {
-    this.previousDate();
-    this.getMediaList();
-  }
-
-  getMediaList() {
-    this.loadingMessage = 'Connecting to Nasa';
-    this._imgsDownloaded = 0;
-
-    this.service.getMediaList({
-      start_date: this.formatDate(this.startDate) ,
-      end_date: this.formatDate(this.endDate)
-    }).subscribe((mediaList: Media[]) => {
-        this.mediaList = mediaList.sort((a, b) => b.date.localeCompare(a.date));
-        this.downloadImages();
-    }, error => {
-      console.error(error);
-      this.loadingMessage = 'Connection Failed.';
+        this.loadImages();
     });
   }
 
-  onMediaSelected(media) {
-    this.service.setMedia(media);
-    this.router.navigate(['/', media.date]);
-  }
-
-  private youtubeImage(url) {
-    const regExp = /embed\/([^)]+)\?/;
-
-    const matches = regExp.exec(url);
-    // const matches = regExp.exec('https://www.youtube.com/embed/B1R3dTdcpSU?rel=0');
-
-    const videoId = matches[1];
-    const thumbnailName = 'maxresdefault.jpg';
-    return `https://img.youtube.com/vi/${videoId}/${thumbnailName}`;
-  }
-
-  set imgsDownloaded(number) {
-    this._imgsDownloaded = number;
-
-    if (this._imgsDownloaded === this.itemsCount) {
-      this.loadImages();
-    }
-  }
-
-  get imgsDownloaded() {
-    return this._imgsDownloaded;
-  }
-
-  get startDate() {
-    return this.formatDate(this.date);
-  }
-
-  get endDate() {
-    const date = new Date(this.startDate);
-
-    date.setDate(this.date.getDate() + this.itemsCount - 1);
-
-    return this.formatDate(date);
-  }
-
-  get imgsReady() {
-    return this.itemsCount === this.imgsDownloaded;
-  }
-
-  private loadImages() {
-    this.mediaList.map(media => {
-      if (media.media_type === 'video') {
-        media.src = this.youtubeImage( media.url);
-      } else {
-        media.src = media.url;
-      }
-      return media;
-    });
+  loadImages() {
 
   }
 
-  private nextDate() {
-    this.date.setDate(this.date.getDate() + this.itemsCount + 1);
-  }
+  // next() {
+  //   this.nextDate();
+  //   this.getMediaList();
+  // }
 
-  private previousDate() {
-    this.date.setDate(this.date.getDate() - this.itemsCount + 1);
-  }
+  // previous() {
+  //   this.previousDate();
+  //   this.getMediaList();
+  // }
 
-  private downloadImages() {
-    this.loadingMessage  = 'Getting Resources';
+  // getMediaList() {
+  //   this.loadingMessage = 'Connecting to Nasa';
+  //   this._downloadedImgs = 0;
 
-    this.mediaList.map((media) => {
+  //   this.service.getMediaList({
+  //     start_date: this.formatDate(this.startDate) ,
+  //     end_date: this.formatDate(this.endDate)
+  //   }).subscribe((mediaList: Media[]) => {
+  //       this.mediaList = mediaList.sort((a, b) => b.date.localeCompare(a.date));
+  //       this.downloadImages();
+  //   }, error => {
+  //     console.error(error);
+  //     this.loadingMessage = 'Connection Failed.';
+  //   });
+  // }
 
-      const image = new Image();
+  // onMediaSelected(media) {
+  //   this.service.setMedia(media);
+  //   this.router.navigate(['/', media.date]);
+  // }
 
-      if (media.media_type === 'video') {
-        image.src = this.youtubeImage( media.url);
-      } else {
-        image.src = media.url;
-      }
+  // private youtubeImage(url) {
+  //   const regExp = /embed\/([^)]+)\?/;
 
-      image.onload = () => {
-        this.imgsDownloaded++;
-      };
+  //   const matches = regExp.exec(url);
+  //   // const matches = regExp.exec('https://www.youtube.com/embed/B1R3dTdcpSU?rel=0');
 
-      return media;
-    });
-  }
+  //   const videoId = matches[1];
+  //   const thumbnailName = 'maxresdefault.jpg';
+  //   return `https://img.youtube.com/vi/${videoId}/${thumbnailName}`;
+  // }
+
+  // set downloadedImgs(number) {
+  //   this._downloadedImgs = number;
+
+  //   if (this._downloadedImgs === this.itemsCount) {
+  //     this.loadImages();
+  //   }
+  // }
+
+  // get downloadedImgs() {
+  //   return this._downloadedImgs;
+  // }
+
+  // get startDate() {
+  //   return this.formatDate(this.date);
+  // }
+
+  // get endDate() {
+  //   const date = new Date(this.startDate);
+
+  //   date.setDate(this.date.getDate() + this.itemsCount - 1);
+
+  //   return this.formatDate(date);
+  // }
+
+  // get imgsReady() {
+  //   return this.itemsCount === this.downloadedImgs;
+  // }
+
+  // private loadImages() {
+  //   this.mediaList.map(media => {
+  //     if (media.media_type === 'video') {
+  //       media.src = this.youtubeImage( media.url);
+  //     } else {
+  //       media.src = media.url;
+  //     }
+  //     return media;
+  //   });
+
+  // }
+
+  // private nextDate() {
+  //   this.date.setDate(this.date.getDate() + this.itemsCount + 1);
+  // }
+
+  // private previousDate() {
+  //   this.date.setDate(this.date.getDate() - this.itemsCount + 1);
+  // }
+
+  // private downloadImages() {
+  //   this.loadingMessage  = 'Getting Resources';
+
+  //   this.mediaList.map((media) => {
+
+  //     const image = new Image();
+
+  //     if (media.media_type === 'video') {
+  //       image.src = this.youtubeImage( media.url);
+  //     } else {
+  //       image.src = media.url;
+  //     }
+
+  //     image.onload = () => {
+  //       this.downloadedImgs++;
+  //     };
+
+  //     return media;
+  //   });
+  // }
 
   private formatDate(date) {
+    date.setDate(date.getDate() + 1);
     return this.datePipe.transform(
       date,
       'yyyy-MM-dd'
